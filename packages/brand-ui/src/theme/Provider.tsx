17fe0470892cdacc4df211e66274cad0ced26113
Robson Oliveira <ropj@cin.ufpe.br>
2022-11-10 12:00:09 -0300

    chore: remove fonts elements and add build folder

--- packages/brand-ui/src/theme/Provider.tsx
+++ packages/brand-ui/src/theme/Provider.tsx
@@ -1,20 +1,15 @@
-import React, { ReactNode, memo } from 'react'
+import type { ReactNode } from 'react'
+import React, { memo } from 'react'
 import { ThemeProvider as BaseProvider } from 'theme-ui'
 
 import { theme } from './theme'
-import Fonts from './Fonts'
 
 interface Props {
   children: ReactNode
 }
 
 function ThemeProvider({ children }: Props) {
-  return (
-    <BaseProvider theme={theme as any}>
-      <Fonts />
-      {children}
-    </BaseProvider>
-  )
+  return <BaseProvider theme={theme as any}>{children}</BaseProvider>
 }
 
 export default memo(ThemeProvider)
